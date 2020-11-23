7b3f816dae2ba508ef0e7576b106fb580356ba27
vishykasinadhuni <viswanadhk@yahoo.com>
2020-11-23 16:47:06 -0500

    exercise-105

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ const express = require('express')
 // Create Express app
 const app = express()
 
+var randomString = function (len, bits)
+{
+    bits = bits || 36;
+    var outStr = "", newStr;
+    while (outStr.length < len)
+    {
+        newStr = Math.random().toString(bits).slice(2);
+        outStr += newStr.slice(0, Math.min(newStr.length, (len - outStr.length)));
+    }
+    return outStr.toUpperCase();
+};
+
+const startHash = randomString(36,32)
+
+
 // A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`Responding with ${randomString(36,32)}`))
 
 // Start the Express server
-app.listen(3000, () => console.log('Server started in port 3000!'))
+app.listen(3000, () => console.log(`Server started with ${startHash}`))
